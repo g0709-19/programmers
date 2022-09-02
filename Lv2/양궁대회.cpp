4832be73505257325c071a253fe640cf9ce4aa41
ma9pe <sch030518@gmail.com>
2022-09-02 14:26:33 +0900

    양궁대회*
    가장 큰 점수차인 경우가 2개 이상일 때
    가장 작은 점수를 많이 맞춘 경우를 걸러내야 했는데,
    반복문에서 ryan > higherCase 인 경우에만 break 를 걸어서
    ryan < higherCase 면 다음 작은 점수를 보게 돼서 오답이 나왔다.

--- Lv2/양궁대회.cpp
+++ Lv2/양궁대회.cpp
@@ -7,66 +7,66 @@ using namespace std;
 vector<int> apeachInfo;
 pair<int, vector<int>> higherCase = {0, vector<int>(1, -1)};
 
-void find(int scoreIndex, int arrow, vector<int> lionInfo) {
-  if (scoreIndex == 11) {
-    lionInfo[scoreIndex-1] += arrow;
-
-    int apeachScore = 0;
-    int lionScore = 0;
-    for (int score=10; score>=0; --score) {
-      int scoreIndex = 10 - score;
-      int apeachCount = apeachInfo[scoreIndex];
-      int lionCount = lionInfo[scoreIndex];
-      if (apeachCount != 0 || lionCount != 0) {
-        if (apeachCount >= lionCount) {
-          apeachScore += score;
-        } else {
-          lionScore += score;
-        }
+int getScoreDifferenceWithApeach(vector<int> &ryanInfo) {
+  int apeachScore = 0;
+  int ryanScore = 0;
+  for (int scoreIndex=0; scoreIndex<=10; ++scoreIndex) {
+    const int SCORE = 10 - scoreIndex;
+    if (apeachInfo[scoreIndex] != 0 || ryanInfo[scoreIndex] != 0) {
+      if (apeachInfo[scoreIndex] >= ryanInfo[scoreIndex]) {
+        apeachScore += SCORE;
+      } else {
+        ryanScore += SCORE;
       }
     }
-    if (lionScore > apeachScore) {
-      int difference = lionScore - apeachScore;
-      if (difference > higherCase.first) {
-        higherCase = {difference, lionInfo};
-      } else if (difference == higherCase.first) {
-        for (int score=0; score<=10; ++score) {
-          int scoreIndex = 10 - score;
-          if (higherCase.second[scoreIndex] < lionInfo[scoreIndex]) {
-            higherCase = {difference, lionInfo};
+  }
+  return ryanScore - apeachScore;
+}
+
+void find(int scoreIndex, int remainedArrows, vector<int> ryanInfo) {
+  if (scoreIndex == 11 || remainedArrows == 0) {
+    ryanInfo[scoreIndex - 1] += remainedArrows;
+    // 점수차 계산
+    int difference = getScoreDifferenceWithApeach(ryanInfo);
+    if (difference > 0) {
+      if (difference == higherCase.first) {
+        // 가장 낮은 점수 큰 쪽으로
+        for (int lowerScoreIndex=10; lowerScoreIndex>=0; --lowerScoreIndex) {
+          if (ryanInfo[lowerScoreIndex] > higherCase.second[lowerScoreIndex]) {
+            higherCase = {difference, ryanInfo};
+            break;
+          } else if (ryanInfo[lowerScoreIndex] < higherCase.second[lowerScoreIndex]) {
             break;
           }
         }
+      } else if (difference > higherCase.first) {
+        higherCase = {difference, ryanInfo};
       }
     }
-
-    lionInfo[scoreIndex-1] -= arrow;
+    ryanInfo[scoreIndex - 1] -= remainedArrows;
   } else {
-    if (arrow > apeachInfo[scoreIndex]) {
-      int usedArrow = apeachInfo[scoreIndex] + 1;
-      lionInfo[scoreIndex] += usedArrow;
-      find(scoreIndex+1, arrow-usedArrow, lionInfo);
-      lionInfo[scoreIndex] -= usedArrow;
+    if (remainedArrows > apeachInfo[scoreIndex]) {
+      ryanInfo[scoreIndex] += (apeachInfo[scoreIndex] + 1);
+      find(scoreIndex + 1, remainedArrows - apeachInfo[scoreIndex] - 1, ryanInfo);
+      ryanInfo[scoreIndex] -= (apeachInfo[scoreIndex] + 1);
     }
-    find(scoreIndex+1, arrow, lionInfo);
+    find(scoreIndex + 1, remainedArrows, ryanInfo);
   }
 }
 
 vector<int> solution(int n, vector<int> info) {
-  vector<int> answer;
   apeachInfo = info;
   find(0, n, vector<int>(11, 0));
-  answer = higherCase.second;
-  return answer;
+  return higherCase.second;
 }
 
 int main() {
-  int n = 10;
-  vector<int> apeachInfo = {0,0,0,0,0,0,0,0,3,4,3};
+  int n = 9;
+  vector<int> apeachInfo = {0,0,1,2,0,1,1,1,1,1,1};
   vector<int> lionInfo = solution(n, apeachInfo);
   for (int lionScore : lionInfo) {
     cout << lionScore << ' ';
   }
-  cout << '\n' << countRecr << '\n';
+  cout << '\n';
   return 0;
 }
