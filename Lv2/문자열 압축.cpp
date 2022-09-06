c9ce413fbb946bfb8d1a88f44bf4b89cab45b0d1
ma9pe <sch030518@gmail.com>
2022-09-06 17:16:41 +0900

    문자열 압축
    count 가 10 이상인 경우에도 1만 더해줘서 오류가 났었다.

--- Lv2/문자열 압축.cpp
+++ Lv2/문자열 압축.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+int getLengthOfNumber(int num) {
+  int length = 1;
+  while (num >= 10) {
+    ++length;
+    num /= 10;
+  }
+  return length;
+}
+
 int solution(string s) {
   int answer = 1001;
   for (int k = 1; k <= s.size(); ++k) {
@@ -14,6 +23,7 @@ int solution(string s) {
       int count = 1;
       bool isNotFound = false;
       string last = s.substr(index, k);
+      // 연속되는 중복 문자열이 있는지 검사
       for (index = index + k; index < s.size(); index += k) {
         string crop = s.substr(index, k);
         if (last == crop) {
@@ -25,10 +35,11 @@ int solution(string s) {
           break;
         }
       }
+      // 중복 문자열 없거나 모든 문자열을 검사했으면 길이 저장
       if (isNotFound || index >= s.size()) {
         length += last.size();
         if (count > 1) {
-          ++length;
+          length += getLengthOfNumber(count);
         }
       }
     }
@@ -39,8 +50,7 @@ int solution(string s) {
 
 int main()
 {
-  // string s = "aabbaccc";
-  string s = "xababcdcdababcdcd";
+  string s = "aabbaccc";
   int answer = solution(s);
   cout << "answer: " << answer << '\n';
   return 0;
